5fc8cb352c9a8ef1fd68f216a12adbd2ce652e66
Shankarchalla171 <internshipchalla171@gmail.com>
2025-05-23 18:57:10 +0530

    added the local storage feature..

--- notes_app/src/context/notes-context.jsx
+++ notes_app/src/context/notes-context.jsx
@@ -1,12 +1,21 @@
-import { useContext, createContext, useReducer } from "react";
+import { useContext, createContext, useReducer, useEffect } from "react";
 import noteReducer from "../reducer/note-reducr";
- export const notesContext = createContext();
 
+export const notesContext = createContext();
+function getinitialState(){
+    const storedstate=localStorage.getItem("state");
+    return storedstate? JSON.parse(storedstate):{ title: "", text: "", notes: [] };
+}
 const NotesProvider = ({ children }) => {
-    const [{ title, text, notes }, dispatch] = useReducer(noteReducer, { title: "", text: "", notes: [] });
+
+    const [state, dispatch] = useReducer(noteReducer, getinitialState());
+
+    useEffect(()=>{
+       localStorage.setItem('state',JSON.stringify(state));
+    },[state])
    return (
     
-    < notesContext.Provider value={{title,text,notes,dispatch}}>
+    < notesContext.Provider value={{...state,dispatch}}>
         {children}
     </notesContext.Provider>
    )
